5e44b45f3736b6181b0749ea73dc833088418328
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-24 22:33:55 +0200

    feat: Store scroll position while changing views.
    Resolves #6.

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ const app = Elm.Main.init({
 
 // Add routing as of https://github.com/elm/browser/blob/1.0.0/notes/navigation-in-elements.md
 
+// Save the scrollY positions between state changes so we can scroll back to it.
+let scrollY = 0;
+
 // Inform app of browser navigation (the BACK and FORWARD buttons)
 window.addEventListener('popstate', function() {
   app.ports.onUrlChange.send(location.href);
+  setTimeout(() => window.scrollTo(0, scrollY), 10);
 });
 
 // Change the URL upon request, inform app of the change.
 app.ports.pushUrl.subscribe(function(url) {
+  scrollY = window.pageYOffset;
   history.pushState({}, '', url);
   app.ports.onUrlChange.send(location.href);
 });

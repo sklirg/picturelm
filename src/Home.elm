0d83170fec38680df31b8e0cec9411afb35fc141
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-25 22:48:48 +0200

    feat: Make github url open in new tab.

--- src/Home.elm
+++ src/Home.elm
@@ -28,7 +28,7 @@ import Css
 import Gallery.View exposing (galleryListView, imageListView, singleImageView)
 import Html
 import Html.Styled exposing (Html, a, div, h1, p, text, toUnstyled)
-import Html.Styled.Attributes exposing (css, href)
+import Html.Styled.Attributes exposing (css, href, target)
 import Model exposing (AppModel)
 import Msg exposing (AppMsg(..))
 import Navigation exposing (Route(..), link)
@@ -141,7 +141,7 @@ footer =
                     , fontWeight (int 300)
                     ]
                 ]
-                [ a [ href "https://github.com/sklirg/picturelm" ] [ text "Image gallery made with ❤ in Elm" ] ]
+                [ a [ href "https://github.com/sklirg/picturelm", target "_blank" ] [ text "Image gallery made with ❤ in Elm" ] ]
             ]
         ]
 

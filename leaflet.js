47ea3304f38f775e5475772e08461051a902c529
Håkon Solbjørg <hakon@solbj.org>
2022-05-16 21:23:14 +0200

    chore: Upgrade leaflet&mapbox

--- leaflet.js
+++ leaflet.js
@@ -26,12 +26,12 @@ function initializeMap(coordinates, tries = 0) {
   map.setView(coordinates, 12);
 
   L.tileLayer(
-    `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${mapboxTilesAccessToken}`,
+    `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${mapboxTilesAccessToken}`,
     {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: "mapbox.streets",
+      id: "mapbox/streets-v11",
       accessToken: mapboxTilesAccessToken
     }
   ).addTo(map);
